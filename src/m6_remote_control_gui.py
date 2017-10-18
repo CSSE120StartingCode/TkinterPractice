b45aacd17edf656288979626f671d63da4653b00
David Fisher <fisherds@rose-hulman.edu>
2017-10-17 23:53:38 -0400

    placeholder follow along exercise

---
+++ src/m6_remote_control_gui.py
@@ -0,0 +1,24 @@
+"""
+This project lets you try out Tkinter/Ttk and practice it!
+
+Authors: David Fisher and PUT_YOUR_NAME_HERE.
+"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+
+import tkinter
+from tkinter import ttk
+
+
+def main():
+    """ Constructs a GUI that will be used MUCH later to control EV3. """
+    root = tkinter.Tk()
+    # ------------------------------------------------------------------
+    # TODO: 2. Follow along with the video to make a remote control GUI
+    # ------------------------------------------------------------------
+
+    root.mainloop()
+
+
+# ----------------------------------------------------------------------
+# Calls  main  to start the ball rolling.
+# ----------------------------------------------------------------------
+main()
