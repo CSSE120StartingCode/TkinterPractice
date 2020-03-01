22a0daa1be40d537f0c6f6cf5a8006052efe677a
Your Name <you@example.com>
2020-03-01 20:52:40 +0200

    some changes

--- more_examples/m1e_label.py
+++ more_examples/m1e_label.py
@@ -18,29 +18,30 @@ def main():
     # Root (main) window
     root = tkinter.Tk()
     root.title('Hello!')
+    root.geometry("300x200")
 
     # Frame
     frame1 = ttk.Frame(root)
-    frame1.grid()
+    frame1.place(relx=0.1,rely=0.1,relheight=0.9,relwidth=0.9)
 
     # Label
-    label = ttk.Label(frame1, text='This is a Label above a Button')
-    label.grid()
+    label = ttk.Label(frame1, text='This is a Label above a Button',anchor="nw",justify="center")
+    label.place(relx=0.1,rely=0.02,relheight=0.1,relwidth=0.9)
 
     # Two buttons
     change_title_button = ttk.Button(frame1,
                                      text='Change the Title (above)')
-    change_title_button.grid()
+    change_title_button.place(relx=0.1,rely=0.1,relheight=0.3,relwidth=0.7)
     change_title_button['command'] = lambda: change_title(root)
 
     quit_button = ttk.Button(frame1, text='Quit')
-    quit_button.grid()
+    quit_button.place(relx=0.2,rely=0.4,relheight=0.15,relwidth=0.4)
     quit_button['command'] = lambda: close_window(root)
 
     # Another Label, with its text set another way
     label2 = ttk.Label(frame1)
     label2['text'] = 'Later, we will put Labels BESIDE Buttons'
-    label2.grid()
+    label2.place(relx=0.1,rely=0.6,relheight=0.1,relwidth=0.7)
 
     root.mainloop()
 
